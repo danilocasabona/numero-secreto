476a8a64fd1f2e60d486484cd7b18c14a116fa3f
Danilo Casabona <danilocasabona>
2024-02-07 19:36:39 -0300

    Alterando o limite para 50 numeros.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numerosLimite = 100;
+let numerosLimite = 50;
 let numeroAleatorio = gerarNumeroAleatorio();
 
 function alterarTextoNaTela(tag, texto){
@@ -9,7 +9,7 @@ function alterarTextoNaTela(tag, texto){
 }
 function exibirMensagemInicial(){
     alterarTextoNaTela('h1', 'Jogo do número secreto!');
-    alterarTextoNaTela('p', 'Escolha um número entre 1 e 100:');
+    alterarTextoNaTela('p', 'Escolha um número entre 1 e 50:');
 }
 
 exibirMensagemInicial();
